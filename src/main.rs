872107f62be4cd7c1965a68e2b17b66a5659ba77
cynic <daniel@szymanski.cc>
2025-06-01 14:19:32 +0200

    Add working blocking example with crossbeam select!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,77 @@
-use crossbeam::channel;
-
 #[tokio::main]
 async fn main() {
+    // variant_try_recv().await;
+    variant_crossbeam_select().await;
+}
+
+#[allow(dead_code)]
+async fn variant_crossbeam_select() {
+    let (tx1, rx1) = crossbeam::channel::unbounded::<&str>();
+    let (tx2, rx2) = crossbeam::channel::unbounded::<&str>();
+    tokio::task::spawn_blocking(move || {
+        println!("Blocking task started!, thread id: {:?}", std::thread::current().id());
+        let mut rx1_closed = false;
+        let mut rx2_closed = false;
+        loop {
+            // println!("inside the loop");
+            crossbeam::channel::select! {
+                    recv(rx1) -> msg => {
+                        match msg {
+                            Ok(msg) => {
+                                println!("rx1: {}", msg);
+                                std::thread::sleep(std::time::Duration::from_secs(3));
+                                println!("rx1 task done after 3s");
+                            }
+                            Err(e) => {
+                                // println!("rx1 error: {}", e);
+                                rx1_closed = true;
+                            }
+                        }
+                    },
+                    recv(rx2) -> msg => {
+                        match msg {
+                            Ok(msg) => {
+                                println!("rx2: {}", msg);
+                            }
+                            Err(e) => {
+                                // println!("rx2 error: {}", e);
+                                rx2_closed = true;
+                            }
+                        }
+                    },
+                    default => std::thread::sleep(std::time::Duration::from_millis(1))
+            }
+            if rx1_closed && rx2_closed {
+                break;
+            }
+        }
+
+        println!("blocking thread finished");
+    });
+
+    println!("Non-blocking task started!, thred id: {:?}", std::thread::current().id());
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    println!("Send to tx1");
+    let _ = tx1.send("Hello from non-blocking task");
+    println!("Send to tx1 finished");
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    println!("Send to tx2");
+    let _ = tx2.send("Hello from non-blocking task tx2");
+    println!("Send to tx2 finished");
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    println!("dropping tx1");
+    drop(tx1);
+    println!("dropped tx1");
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    println!("dropping tx2");
+    drop(tx2);
+    println!("dropped tx2");
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    println!("Non-blocking task completed!");
+}
+
+#[allow(dead_code)]
+async fn variant_try_recv() {
     let (tx1, mut rx1) = tokio::sync::mpsc::channel(20);
     let (tx2, mut rx2) = tokio::sync::mpsc::channel(20);
     tokio::task::spawn_blocking(move || {
@@ -39,10 +109,6 @@ async fn main() {
                 std::thread::sleep(std::time::Duration::from_millis(5));
             }
         }
-
-        // crossbeam::channel::select! {
-        //         recv(rx1) -> _ => println!("kdfjld")
-        // }
 
         println!("blocking thread finished");
     });
